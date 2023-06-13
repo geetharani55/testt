c4d2910a8c6c4ffb0784c557cefc0640d0fdc53f
geethrani55 <geethamvdlgs@gmail.com>
2023-06-13 10:22:22 +0530

    added

---
+++ Area_Square.java
@@ -0,0 +1,14 @@
+package square1;
+
+public class Area_Square 
+{
+		 public static void main(String[] kk)
+		 {
+			  int a,a1,c;
+			  a=4;
+			  a1=4;
+			  c=a*a1;
+	//java print
+			  System.out.println("Area of Square is "+c);
+		 }  
+}
